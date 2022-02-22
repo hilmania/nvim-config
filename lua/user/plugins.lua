11c4611d9d29c6f09b1e4a319d75d1a9f8ceccec
Hilman Ibnu <hia.develop@gmail.com>
2022-02-22 11:17:32 +0700

    [update] plugin project, whichkey, and autocommands

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -51,10 +51,12 @@ return packer.startup(function(use)
   use "akinsho/bufferline.nvim"
   use "moll/vim-bbye"
   use "akinsho/toggleterm.nvim"
+  use "ahmedkhalf/project.nvim"
   use 'lewis6991/impatient.nvim'
   use "lukas-reineke/indent-blankline.nvim"
   use 'goolord/alpha-nvim'
   use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
+  use "folke/which-key.nvim"
 
   --colorscheme
   use "lunarvim/darkplus.nvim"
